d012e552a3204e6d4a91644089a0799bd22c5bae
5474454 <312163260@qq.com>
2020-05-25 18:47:38 +0800

    first commit

---
+++ server/public/build/precache-manifest.964a51fa9d220c39830cae0f9e520f29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d69a4d46e2b7ea8d4e3f945899f7a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86da5ce7a6d9a4d46a47",
+    "url": "/static/css/2.81187608.chunk.css"
+  },
+  {
+    "revision": "9ec5f57f29c05fe70fce",
+    "url": "/static/css/main.c42e9bc3.chunk.css"
+  },
+  {
+    "revision": "86da5ce7a6d9a4d46a47",
+    "url": "/static/js/2.b97aae36.chunk.js"
+  },
+  {
+    "revision": "9ec5f57f29c05fe70fce",
+    "url": "/static/js/main.85e65fec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4e877ba946a7e6a898ced905611c528",
+    "url": "/static/media/defaultposter.b4e877ba.png"
+  }
+]);
